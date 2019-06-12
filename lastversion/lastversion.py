af16e8ad07cfa85c981638bafb1c8942e2cdade6
Danila <ciapnz@gmail.com>
2019-06-12 15:06:23 +0300

    New --format option and non-GitHub cases

--- lastversion/lastversion.py
+++ lastversion/lastversion.py
@@ -9,6 +9,7 @@ import argparse
 import sys
 import os
 import re
+import json
 from bs4 import BeautifulSoup
 from packaging.version import Version, InvalidVersion
 
@@ -22,69 +23,94 @@ def sanitize_version(version):
         return version.strip()
 
 
-def latest(repo, sniff=True, validate=True):
+def latest(repo, sniff=True, validate=True, format='version'):
 
+    # data that we may collect further
     version = None
+    description = None
+    # set this when an API returns json
+    data = None
 
-    if repo.startswith('https://github.com/'):
-        repo = "/".join(repo.split('/')[3:5])
+    # Some special non-Github cases for our repository are handled by checking URL
 
-    if sniff:
+    # 1. nginx version is taken as version of stable (written by rpm check script)
+    # to /usr/local/share/builder/nginx-stable.ver
+    if repo.startswith('http://nginx.org/') or repo.startswith('https://nginx.org/'):
+        with open('/usr/local/share/builder/nginx-stable.ver', 'r') as file:
+            version = file.read().replace('\n', '')
 
-        # Start by fetching HTML of releases page (screw you, Github!)
+    # 2. monit version can be obtained from Bitbucket downloads section of the project
+    elif repo.startswith('https://mmonit.com/'):
+        # Special case Monit repo
         response = requests.get(
-            "https://github.com/{}/releases".format(repo),
+            "https://api.bitbucket.org/2.0/repositories/{}/downloads".format("tildeslash/monit"),
             headers={'Connection': 'close'})
-        html = response.text
+        data = response.json()
+        version = sanitize_version(data['values'][0]['name'])
 
-        soup = BeautifulSoup(html, 'lxml')
+    # 3. Everything else is GitHub passed as owner/repo
+    else:
+        # But if full link specified, strip it to owner/repo
+        if repo.startswith('https://github.com/'):
+            repo = "/".join(repo.split('/')[3:5])
 
-        r = soup.find(class_='release-entry')
-        while r:
-            # this tag is known to hold collection of releases not exposed through API
-            if 'release-timeline-tags' in r['class']:
-                for release in r.find_all(class_='release-entry', recursive=False):
-                    the_version = release.find("a").text
-                    the_version = sanitize_version(the_version)
-                    # check if version is ok and not a prerelease; move on to next tag otherwise
-                    if validate:
-                        try:
-                            v = Version(the_version)
-                            if not v.is_prerelease:
-                                version = the_version
-                                break
-                        except InvalidVersion:
-                            # move on to next thing to parse it
-                            continue
-                    else:
+        if sniff:
+
+            # Start by fetching HTML of releases page (screw you, Github!)
+            response = requests.get(
+                "https://github.com/{}/releases".format(repo),
+                headers={'Connection': 'close'})
+            html = response.text
+
+            soup = BeautifulSoup(html, 'lxml')
+
+            r = soup.find(class_='release-entry')
+            while r:
+                # this tag is known to hold collection of releases not exposed through API
+                if 'release-timeline-tags' in r['class']:
+                    for release in r.find_all(class_='release-entry', recursive=False):
+                        the_version = release.find("a").text
+                        the_version = sanitize_version(the_version)
+                        # check if version is ok and not a prerelease; move on to next tag otherwise
+                        if validate:
+                            try:
+                                v = Version(the_version)
+                                if not v.is_prerelease:
+                                    version = the_version
+                                    break
+                            except InvalidVersion:
+                                # move on to next thing to parse it
+                                continue
+                        else:
+                            version = the_version
+                            break
+                else:
+                    # formal release
+                    label_latest = r.find(class_='label-latest', recursive=False)
+                    if label_latest:
+                        the_version = r.find(class_='css-truncate-target').text
+                        the_version = sanitize_version(the_version)
+                        # trust this to be the release and validate below
                         version = the_version
+                        description = r.find(class_='markdown-body').text
                         break
+                r = r.find_next_sibling(class_='release-entry', recursive=False)
+
+        if not version:
+            headers = {'Connection': 'close'}
+            api_token = os.getenv("GITHUB_API_TOKEN")
+            if api_token:
+                headers['Authorization'] = "token {}".format(api_token)
+
+            r = requests.get(
+                'https://api.github.com/repos/{}/releases/latest'.format(repo),
+                headers=headers)
+            if r.status_code == 200:
+                version = r.json()['tag_name']
+                version = sanitize_version(version)
             else:
-                # formal release
-                label_latest = r.find(class_='label-latest', recursive=False)
-                if label_latest:
-                    the_version = r.find(class_='css-truncate-target').text
-                    the_version = sanitize_version(the_version)
-                    # trust this to be the release and validate below
-                    version = the_version
-                    break
-            r = r.find_next_sibling(class_='release-entry', recursive=False)
-
-    if not version:
-        headers = {'Connection': 'close'}
-        api_token = os.getenv("GITHUB_API_TOKEN")
-        if api_token:
-            headers['Authorization'] = "token {}".format(api_token)
-
-        r = requests.get(
-            'https://api.github.com/repos/{}/releases/latest'.format(repo),
-            headers=headers)
-        if r.status_code == 200:
-            version = r.json()['tag_name']
-            version = sanitize_version(version)
-        else:
-            sys.stderr.write(r.text)
-            return None
+                sys.stderr.write(r.text)
+                return None
 
     if validate:
         try:
@@ -93,21 +119,33 @@ def latest(repo, sniff=True, validate=True):
             sys.stderr.write('Got invalid version: {}'.format(version))
             return None
 
-    # return the version if we've reached far enough:
-    return version
+    # return the release if we've reached far enough:
+    if format == 'version':
+        return version
+    elif format == 'json':
+        if not data:
+            data = {}
+        if description:
+            description = description.strip()
+        data['version'] = version
+        data['description'] = description
+        return json.dumps(data)
 
 
 def main():
 
     parser = argparse.ArgumentParser(description='Get latest release from GitHub.')
-    parser.add_argument('repo', metavar='R',
+    parser.add_argument('repo', metavar='REPOSITORY',
                         help='GitHub repository in format owner/name')
     parser.add_argument('--nosniff', dest='sniff', action='store_false')
     parser.add_argument('--novalidate', dest='validate', action='store_false')
-    parser.set_defaults(sniff=True, validate=True)
+    parser.add_argument('--format',
+                        choices=['json', 'version'],
+                        help='Output format')
+    parser.set_defaults(sniff=True, validate=True, format='version')
     args = parser.parse_args()
 
-    version = latest(args.repo, args.sniff, args.validate)
+    version = latest(args.repo, args.sniff, args.validate, args.format)
 
     if version:
         print(version)
