8d5f3cb7a93c2c5ea454d91ac8589ee970d8a00d
Danila Vershinin <ciapnz@gmail.com>
2021-06-28 00:21:48 +0300

    Cleanup

--- lastversion/WikipediaRepoSession.py
+++ lastversion/WikipediaRepoSession.py
@@ -1,10 +1,18 @@
 from dateutil import parser
 from bs4 import BeautifulSoup
 import logging
+from .ProjectHolder import ProjectHolder
 
 log = logging.getLogger(__name__)
 
-from .ProjectHolder import ProjectHolder
+
+def remove_words(title):
+    parts = title.split(' ')
+    parts_n = []
+    for part in parts:
+        if not part.isalpha():
+            parts_n.append(part)
+    return " ".join(parts_n)
 
 
 class WikipediaRepoSession(ProjectHolder):
@@ -54,34 +62,10 @@ class WikipediaRepoSession(ProjectHolder):
             self.hostname = self.DEFAULT_HOSTNAME
         self.set_repo(repo)
 
-    def remove_words(self, title):
-        parts = title.split(' ')
-
-        # TODO test v5.12-rc1-dontuse -> v5.12.rc1
-        # go through parts separated by dot, detect beta level, and weed out numberless info:
-        parts_n = []
-        for part in parts:
-            # help devel releases to be correctly identified
-            # https://www.python.org/dev/peps/pep-0440/#developmental-releases
-            if part in ['devel', 'test', 'dev']:
-                part = 'dev0'
-            else:
-                # help post (patch) releases to be correctly identified (e.g. Magento 2.3.4-p2)
-                # p12 => post12
-                import re
-                part = re.sub('^p(\\d+)$', 'post\\1', part, 1)
-            if not part.isalpha():
-                parts_n.append(part)
-        return " ".join(parts_n)
-
     def get_latest(self, pre_ok=False, major=None):
         ret = None
         tag_name = None
         tag = {}
-        # to leverage cachecontrol, we fetch the feed using requests as usual
-        # then feed the feed to feedparser as a raw string
-        # e.g. https://sourceforge.net/projects/keepass/rss?path=/
-        # TODO this could be better. Now it is actually checking versions in topmost files
         r = self.get('https://{}/wiki/{}'.format(self.hostname, self.repo))
         soup = BeautifulSoup(r.text, "html.parser")
         # we only need the first one
@@ -95,8 +79,8 @@ class WikipediaRepoSession(ProjectHolder):
                 published = parser.parse(published)
                 for t in release_data.select('sup, span'):
                     t.decompose()
-                tag_name = self.remove_words(release_data.text).split('/')[0]
                 # remove alphas from beginning
+                tag_name = remove_words(release_data.text).split('/')[0]
                 tag['title'] = release_data.text
                 log.info('Pre-parsed title: {}'.format(tag['title']))
                 tag['tag_date'] = published
