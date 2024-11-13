ab71995bb9132041cbd04e1c584430774da2340d
Danila Vershinin <ciapnz@gmail.com>
2024-11-14 00:37:17 +0800

    Fix Python 3.6 support

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ install_requires = [
     # https://github.com/ionrock/cachecontrol/issues/137
     "requests>=2.6.1",
     "packaging",
-    "cachecontrol[filecache]",
+    "cachecontrol[filecache]==0.12.14; python_version < '3.7'",
+    "cachecontrol[filecache]; python_version >= '3.7'",
     "appdirs",
     "feedparser",
     "python-dateutil",
